624f8276af9b90baab7223ffe9824453007e2f4b
Hugo Aguirre <bul.ikana@gmail.com>
2018-10-13 23:10:07 -0500

    Add vue router

--- code.js
+++ code.js
@@ -1,7 +1,37 @@
+//                      //
+// Component definition //
+//                      //
+
+// Pages //
+
+// Book Selection Page
+const bookSelectionPage = Vue.component('book-selection', {
+  template: '#book-selection'
+})
+
+//                   //
+// Router definition //
+//                   //
+
+const router = new VueRouter ({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: bookSelectionPage
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ]
+})
+
 //                //
 // Vue definition //
 //                //
 
 var vm = new Vue({
-  el: '#app'
-});
+  el: '#app',
+  router: router
+})
